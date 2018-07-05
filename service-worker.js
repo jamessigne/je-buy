98e9d612f9af797ec531151f4eb8d1fc704aa93b
carter <abel.james91@gmail.com>
2018-07-05 15:00:46 +0100

    https

--- service-worker.js
+++ service-worker.js
@@ -24,14 +24,16 @@ self.toolbox.precache(
 );
 
 // dynamically cache any other local assets
-// /\/(^(?!api).)*/
-//self.toolbox.router.any('http://35.226.130.188', self.toolbox.fastest);
+// (^(?!api).)
+//(^(?!https:\/\/storage.googleapis.com\/jebuy_images\/).) pour les images
+//
+//self.toolbox.router.any('(^(?!https://storage.googleapis.com/jebuy_images/))', self.toolbox.fastest);
 
 // for any other requests go to the network, cache,
 // and then only use that cached resource if your user goes offline
 self.toolbox.router.default = self.toolbox.networkFirst;
 
-self.addEventListener('install', function(event) {
+/* self.addEventListener('install', function(event) {
  
    
   try{
@@ -45,4 +47,4 @@ self.addEventListener('install', function(event) {
   }catch (ex){
     console.log(ex.message)
   }
-});
+}); */
